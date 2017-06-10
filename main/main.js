64f1bb690633fb55d607b3ab8b9390c058dc27fb
CANSTER <921163572@qq.com>
2017-06-10 15:04:41 +0800

    3

--- main/main.js
+++ main/main.js
@@ -1,12 +1,6 @@
 // Write your cade below:
 
- function main(a,b){
- var result=a%b;
-return result; 
-} ;
-var m=main(9,2);
+var m=9%4; 
 console.log(m);
-module.exports = function main(a,b) {
-result=a%b;
-  
+module.exports = function main() {
 };
